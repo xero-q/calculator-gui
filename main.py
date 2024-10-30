1d8c1eae57a168c79fde16fe07a2f77d86154511
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-10-30 17:43:51 -0400

    Improved the positions of the digit buttons

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 from tkinter import *
 from tkinter import ttk
 from utils.calculator import Calculator
+import tkinter as tk
 
 # Create an instance of tkinter frame
 win= Tk()
 
 # Set the size of the tkinter window
 win.geometry("700x350")
+win.resizable(False, False)
 
 calculator = Calculator()
 operand_left = None
@@ -24,8 +26,9 @@ def update_value():
 
 entry = Entry(win, font=('Arial', 14))
 entry.pack(pady=10)
+# entry.config(state="disabled")
 
-def add_digit(digit: int):
+def add_digit(digit):
     global just_added_operator
     if not just_added_operator:
         calculator.input_digit(digit)
@@ -68,6 +71,21 @@ def operate_subtract():
        operator = '-' 
     just_added_operator = True   
 
+def operate_multiply():
+    global operand_left, operand_right, operator, just_added_operator
+    if operand_left is None:        
+        operator = '*'
+        operand_left = calculator.value
+    elif operator == '*':
+       operand_right = calculator.value
+       calculator.value = operand_left
+       calculator.multiply(operand_right)
+       operand_left = calculator.value
+       update_value()
+    else:
+        operator = '*'
+    just_added_operator = True    
+
 def operate_equal():
     global operand_left, operand_right, operator
     if operand_left is not None and operand_right is not None:
@@ -80,6 +98,10 @@ def operate_equal():
                 calculator.value = operand_left
                 calculator.subtract(operand_right)
                 update_value()
+            case '*':
+                calculator.value = operand_left
+                calculator.multiply(operand_right)
+                update_value()
     elif operand_left is not None and operator:
         operand_right = calculator.value
         match operator:
@@ -91,28 +113,31 @@ def operate_equal():
                 calculator.value = operand_left
                 calculator.subtract(operand_right)
                 update_value()
+            case '*':
+                calculator.value = operand_left
+                calculator.multiply(operand_right)
+                update_value()
 
     operand_left = calculator.value
     operand_right = None
     operator = ''
 
-# Create a Button to display the message
-ttk.Button(win, text= "0", command=lambda: add_digit(0)).pack(pady= 20)
-ttk.Button(win, text= "1", command=lambda: add_digit(1)).pack(pady= 20)
-ttk.Button(win, text= "2", command=lambda: add_digit(2)).pack(pady= 20)
-ttk.Button(win, text= "3", command=lambda: add_digit(3)).pack(pady= 20)
-ttk.Button(win, text= "4", command=lambda: add_digit(4)).pack(pady= 20)
-ttk.Button(win, text= "5", command=lambda: add_digit(5)).pack(pady= 20)
-ttk.Button(win, text= "6", command=lambda: add_digit(6)).pack(pady= 20)
-ttk.Button(win, text= "7", command=lambda: add_digit(7)).pack(pady= 20)
-ttk.Button(win, text= "8", command=lambda: add_digit(8)).pack(pady= 20)
-ttk.Button(win, text= "9", command=lambda: add_digit(9)).pack(pady= 20)
+
+positions_buttons = [(160,190),(100,100),(130,100),(160,100),(100,130),(130,130),(160,130),(100,160),(130,160),(160,160)]   
+
+for i in range(len(positions_buttons)):
+    button_i = tk.Button(win, text= str(i))
+    button_i.pack(pady=20)
+    button_i.place(x=positions_buttons[i][0],y=positions_buttons[i][1],width=25,height=25)
+    button_i.config(command=lambda digit=i: add_digit(digit))
 
 ttk.Button(win, text= "CE", command=restart_calculator).pack(pady= 20)
 
 ttk.Button(win, text= "+", command=operate_add).pack(pady= 20)
 
 ttk.Button(win, text= "-", command=operate_subtract).pack(pady= 20)
+
+ttk.Button(win, text= "*", command=operate_multiply).pack(pady= 20)
 
 ttk.Button(win, text= "=", command=operate_equal).pack(pady= 20)
 
