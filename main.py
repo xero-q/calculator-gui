8d06d5bb4214d27dfda7c3a19a7d0047434bf496
Anibal Sanchez <anibal.sanchez@dspot.com.pl>
2024-10-31 12:08:47 -0400

    Improved positioning of all buttons

--- main.py
+++ main.py
@@ -33,25 +33,44 @@ entry.bind("<Key>", deny_manual_input)
 calculator_gui = CalculatorInterface(calculator)
 calculator_gui.attach(GUIObserver())
 
-positions_buttons = [(160,190),(100,100),(130,100),(160,100),(100,130),(130,130),(160,130),(100,160),(130,160),(160,160)]   
+positions_buttons = [(100,220),(100,180),(140,180),(180,180),(100,140),(140,140),(180,140),(100,100),(140,100),(180,100)]   
 
 for i in range(len(positions_buttons)):
     button_i = tk.Button(win, text= str(i))
     button_i.pack(pady=20)
-    button_i.place(x=positions_buttons[i][0],y=positions_buttons[i][1],width=25,height=25)
+    if i == 0:
+        button_i.place(x=positions_buttons[i][0],y=positions_buttons[i][1],width=70,height=30)
+    else:
+        button_i.place(x=positions_buttons[i][0],y=positions_buttons[i][1],width=30,height=30)
     button_i.config(command=lambda digit=i: calculator_gui.add_digit(digit))
 
-ttk.Button(win, text= "CE", command=calculator_gui.restart).pack(pady= 20)
+button_ce = tk.Button(win, text= "CE", command=calculator_gui.restart)
+button_ce.pack(pady= 20)
+button_ce.place(x=180,y=220,width=30,height=30)
 
-ttk.Button(win, text= "+", command=calculator_gui.operate_add).pack(pady= 20)
+button_plus = tk.Button(win, text= "+", command=calculator_gui.operate_add)
+button_plus.pack(pady= 20)
+button_plus.place(x=220,y=100,width=30,height=70)
 
-ttk.Button(win, text= "-", command=calculator_gui.operate_subtract).pack(pady= 20)
+button_equal = tk.Button(win, text= "=", command=calculator_gui.operate_equal)
+button_equal.pack(pady= 20)
+button_equal.place(x=220,y=180,width=30,height=70)
 
-ttk.Button(win, text= "*", command=calculator_gui.operate_multiply).pack(pady= 20)
+button_percent = tk.Button(win, text= "%", command=calculator_gui.operate_divide)
+button_percent.pack(pady= 20)
+button_percent.place(x=100,y=60,width=30,height=30)
 
-ttk.Button(win, text= "/", command=calculator_gui.operate_divide).pack(pady= 20)
+button_divide = tk.Button(win, text= "/", command=calculator_gui.operate_divide)
+button_divide.pack(pady= 20)
+button_divide.place(x=140,y=60,width=30,height=30)
 
-ttk.Button(win, text= "=", command=calculator_gui.operate_equal).pack(pady= 20)
+button_multiply = tk.Button(win, text= "*", command=calculator_gui.operate_multiply)
+button_multiply.pack(pady= 20)
+button_multiply.place(x=180,y=60,width=30,height=30)
+
+button_subtract = tk.Button(win, text= "-", command=calculator_gui.operate_subtract)
+button_subtract.pack(pady= 20)
+button_subtract.place(x=220,y=60,width=30,height=30)
 
 calculator_gui.notify()
 
